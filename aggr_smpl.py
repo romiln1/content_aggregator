309b8a6b44bf06789e8008031483e07b3c137775
mwangi victor <tmvicbvn@gmail.com>
2020-09-08 11:34:03 +0300

    Update aggr_smpl.py

--- aggr_smpl.py
+++ aggr_smpl.py
@@ -1,13 +1,10 @@
-import beautifulsoup
-import notify2
 import json
+import notify2
+import beautifulsoup
 
 urlclient = urlreq(fullscrapadr)
-
 urlpg = urlclient.read()
 
 urlclient.close()
-
 urldata = bs4.BeautifulSoup(urlpg, 'html.parser')
-
-tagbreakdown = str(urldata.find(scrtagcapture).text.strip())
+urldata.find(scrtagcapture).text.strip()
