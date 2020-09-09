2d8f836579c9ac541c530d60b5ab640672ab5fb6
mwangi victor <tmvicbvn@gmail.com>
2020-09-09 11:47:56 +0300

    Update aggr_smpl.py

--- aggr_smpl.py
+++ aggr_smpl.py
@@ -2,9 +2,18 @@ import json
 import notify2
 import beautifulsoup
 
+def alert(info):
+	notify2.init('content aggr')
+        notif = notify2.Notification('content', info)
+	# notif.set_urgency(notify2.URGENCY_CRITICAL)
+        notif.show()
+        notif.set_timeout(10)
+
 urlclient = urlreq(fullscrapadr)
 urlpg = urlclient.read()
 
 urlclient.close()
 urldata = bs4.BeautifulSoup(urlpg, 'html.parser')
 urldata.find(scrtagcapture).text.strip()
+
+alert(--)
