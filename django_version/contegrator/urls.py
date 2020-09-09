26b2b6f59b631c2906f25ecedd2a66774adb98d4
mwangi victor <tmvicbvn@gmail.com>
2020-09-09 11:58:29 +0300

    Delete urls.py

--- django_version/contegrator/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from news import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name = "home"),
-]
